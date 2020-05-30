6520347a390b17c24e8947f298432e0e166cb226
svasenkov <aanher@gmail.com>
2020-05-30 14:42:24 +0300

    1. Commit before add video

---
+++ src/test/java/tests/GoogleTests.java
@@ -0,0 +1,56 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+@Epic("QA.GURU automation course")
+@Story("Google tests with video")
+@Tag("google")
+class GoogleTests {
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true));
+
+        if(System.getProperty("selenoid_url") != null) {
+            Configuration.remote = "http://" + System.getProperty("selenoid_url") + ":4444/wd/hub/";
+        }
+    }
+
+    @Test
+    @Description("Google test, we look for lepra")
+    @DisplayName("Successful search for lepra in google")
+    void successfulSearch() {
+        open("http://google.com");
+
+        $(byName("q")).val("lepra").pressEnter();
+
+        $("html").shouldHave(text("World Health Organization"));
+    }
+
+    @Test
+    @Description("Google test, we look for 2ch, but cant find")
+    @DisplayName("Unsuccessful search for lepra in google")
+    void unSuccessfulSearch() {
+        open("http://google.com");
+
+        $(byName("q")).val("lepra").pressEnter();
+
+        $("html").shouldNotHave(text("World Health Organization"));
+    }
+
+}
